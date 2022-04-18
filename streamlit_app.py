50db2eaf3aceaff9fb787dc67dba447e4429e5e8
Ever-manoj <101855534+Ever-manoj@users.noreply.github.com>
2022-04-18 17:22:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Mom\'s  New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamit.text('Hard-Boiled Free-Range Egg')
