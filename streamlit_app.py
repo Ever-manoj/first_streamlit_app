fb8f95306b36bfde3d5860fa029bf851b64d7dfa
Ever-manoj <101855534+Ever-manoj@users.noreply.github.com>
2022-04-24 19:56:29 +0530

    using import requests api package for frutiyvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,4 +14,6 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect('Pick some fruits :',list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
-
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
