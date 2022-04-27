cbaa6e947682bb36ae53626a04f9d00cbf0d76e5
Ever-manoj <101855534+Ever-manoj@users.noreply.github.com>
2022-04-27 10:55:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,12 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
+
+import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("show tables")
+my_data_row = my_cur.fetchone()
+streamlit.text("Table data contains")
+streamlit.text(my_data_row)
+
