caa42ee6e86f36e47b290e4fa9741d0a54076558
Ever-manoj <101855534+Ever-manoj@users.noreply.github.com>
2022-04-27 12:25:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,16 +28,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #Output
 streamlit.dataframe(fruityvice_normalized)
 
-streamlit.header('Fruityvice Fruit Advice !')
-fruit_choice = streamlit.text_input('what food would you like information about ?','kiwi')
-streamlit.write('The user entered', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
-
-
-streamlit.stop()
 
 streamlit.header('Fruityvice Fruit Advice !')
 try:
@@ -51,7 +42,39 @@ try:
 
 except URLError as e:
     streamlit.error()         
+    
+    
+    
+streamlit.header('Fruityvice Fruit Advice Using function')
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
+streamlit.header('Fruityvice Fruit Advice function!')
+try:
+    fruit_choice = streamlit.text_input('what food would you like information about ?')
+    if not fruit_choice:
+        streamlit.error('Please select a fruit to get a information')
+    else:
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
+
+except URLError as e:
+    streamlit.error()         
+    
                                         
+streamlit.stop()
+
+streamlit.header('Fruityvice Fruit Advice !')
+fruit_choice = streamlit.text_input('what food would you like information about ?','kiwi')
+streamlit.write('The user entered', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
+
+
 #streamlit.stop()
 
 #import pandas
